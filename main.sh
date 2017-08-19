19c89ad06b1a22a231366b01a84441b7819dae78
harper357 <harper357@gmail.com>
2017-08-19 12:53:40 -0700

    Comment&cleanup
    I added a bunch of comments and cleaned up the code some. It could probably stand to be cleaned up more.

--- main.sh
+++ main.sh
@@ -6,29 +6,40 @@ do
   # First we align to a host so we can subtract host reads 
   magicblast13 -sra $sra -db references/human -num_threads 12 -score 50 -penalty -3 -out ~/references/$sra.human.sam
   samtools fasta -f 4 ~/refereneces/$sra.human.sam -1 tmp_read_one -2 tmp_read_two -0 tmp_read_zero
-  # determine if SRA is PE or SE. then run magicblast on the nonhost reads
-  READ1COUNT= wc -l $tmp_read_one
-  READ2COUNT= wc -l $tmp_read_two
-  READ0COUNT= wc -l $tmp_read_zero
+  # should we delete the SAM? 
+  # determine if SRA is PE or SE. then run magicblast on the nonhost reads. This could be done by looking at the meta data, too
+  read1_count= wc -l $tmp_read_one
+  read2_count= wc -l $tmp_read_two
+  read0_count= wc -l $tmp_read_zero
+
   # run magicblast using the CARD gene_homology 
-  if [$READ0COUNT -lt $READ1COUNT -a $READ1COUNT -eq $READ2COUT ]
-  then
-	fastx_clipper -i $tmp_read_one -o $tmp_read_one_trimmed
-	fastx_clipper -i $tmp_read_two -o $tmp_read_two_trimmed
-	magicblast13 -num_threads 12  -infmt fasta -query $tmp_read_one_trimmed -query_mate $tmp_read_two_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_gene.sam -db ~/references/CARD_gene
-  else [$READ1COUNT -lt $READ0COUNT -a -s $READ0COUNT ]
-  then
-	fastx_clipper -i $tmp_read_zero -o $tmp_read_zero_trimmed
-	magicblast13 -num_threads 12 -infmt fasta -query $tmp_read_zero_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_gene.sam -db ~/references/CARD_gene  
-  samtools view -bS $OUT_DIR/$sra.CARD_gene.sam | samtools sort - $OUT_DIR/$sra.CARD_gene.bam
-  rm 
+  if [$read0_count -lt $read1_count -a $read1_count -eq $read2_count ]; then
+	  fastx_clipper -i $tmp_read_one -o $tmp_read_one_trimmed # is this line still needed? 
+	  fastx_clipper -i $tmp_read_two -o $tmp_read_two_trimmed # is this line still needed? 
+    # should we sync the FASTQs here?
+	  magicblast13 -num_threads 12  -infmt fasta -query $tmp_read_one_trimmed -query_mate $tmp_read_two_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_gene.sam -db ~/references/CARD_gene
+  else [$read1_count -lt $read0_count -a -s $read0_count ]; then
+	  fastx_clipper -i $tmp_read_zero -o $tmp_read_zero_trimmed
+	  magicblast13 -num_threads 12 -infmt fasta -query $tmp_read_zero_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_gene.sam -db ~/references/CARD_gene  
+  fi
+
+  # Convert the gene_homology aligned SAM to BAM and sort. Delete SAM to save space
+  samtools view -bS $OUT_DIR/$sra.CARD_gene.sam | samtools sort - $OUT_DIR/$sra.CARD_gene.bam # should we output the unaligned reads to a different bam?
+  rm $OUT_DIR/$sra.CARD_gene.sam
+  
+
   # run magic blast using CARD gene_variant
-  if [$READ0COUNT -lt $READ1COUNT -a $READ1COUNT -eq $READ2COUT ]
-  then
+  if [$read0_count -lt $read1_count -a $read1_count -eq $read2_count ]; then
     magicblast13 -num_threads 12 -infmt fasta -query $tmp_read_one_trimmed -query_mate $tmp_read_two_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_variant.sam -db ~/references/CARD_variant
-  else [$READ1COUNT -lt $READ0COUNT -a -s $READ0COUNT ]
-  then
+  else [$read1_count -lt $read0_count -a -s $read0_count ]; then
     magicblast13 -num_threads 12 -infmt fasta -query $tmp_read_zero_trimmed -score 50 -penalty -3 -out $OUT_DIR/$sra.CARD_variant.sam -db ~/references/CARD_variant
+  fi
+
+  # Convert the gene_variant aligned SAM to BAM and sort. Delete SAM to save space
+  samtools view -bS $OUT_DIR/$sra.CARD_variant.sam | samtools sort - $OUT_DIR/$sra.CARD_variant.bam # should we output the unaligned reads to a different bam?
+  rm $OUT_DIR/$sra.CARD_variant.sam
+
   # process above bamfiles
   
+
 done > folderOfFinal_output
